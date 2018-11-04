c52bc441db02ec0a24d56d94f6946d662d27b33e
gz <gz222@126.com>
2018-11-04 23:51:02 +0800

    add the method hook lib

---
+++ method-hook/src/main/java/com/codetend/method_hook/MethodHook.java
@@ -0,0 +1,59 @@
+package com.codetend.method_hook;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by Administrator on 2018/11/4.
+ */
+
+public class MethodHook {
+    public static void m1() {
+    }
+
+    public static void m2() {
+    }
+
+    private Method srcMethod;
+    private Method hookMethod;
+
+    private long backupMethodPtr;
+
+    public MethodHook(Method src, Method dest) {
+        srcMethod = src;
+        hookMethod = dest;
+        srcMethod.setAccessible(true);
+        hookMethod.setAccessible(true);
+    }
+
+    public void hook() {
+        if (backupMethodPtr == 0) {
+            backupMethodPtr = hook_native(srcMethod, hookMethod);
+        }
+    }
+
+    public void restore() {
+        if (backupMethodPtr != 0) {
+            restore_native(srcMethod, backupMethodPtr);
+            backupMethodPtr = 0;
+        }
+    }
+
+    public void callOrigin(Object receiver, Object... args) throws InvocationTargetException, IllegalAccessException {
+        if (backupMethodPtr != 0) {
+            restore();
+            srcMethod.invoke(receiver, args);
+            hook();
+        } else {
+            srcMethod.invoke(receiver, args);
+        }
+    }
+
+    private static native long hook_native(Method src, Method dest);
+
+    private static native Method restore_native(Method src, long methodPtr);
+
+    static {
+        System.loadLibrary("method-h");
+    }
+}
